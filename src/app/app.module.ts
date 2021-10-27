8f0dddf9760a56ea80683c27fab5dfb46110b7b1
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-27 16:56:11 +0300

    scroll snap css for diff sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section3Component } from './components/section3/section3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component
   ],
   imports: [
     BrowserModule,
