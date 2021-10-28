8a7b45a384e84ff0e5bf4ae05b3fdd2748e89ee3
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-28 05:22:13 +0300

    angular owl carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Section1Component } from './components/section1/section1.component';
 import { Section2Component } from './components/section2/section2.component';
 import { Section3Component } from './components/section3/section3.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Section1Component,
     Section2Component,
-    Section3Component
+    Section3Component,
+    CarouselComponent
   ],
   imports: [
+    CarouselModule,
     BrowserModule,
     AppRoutingModule
   ],
